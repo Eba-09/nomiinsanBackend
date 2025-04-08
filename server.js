ca4a351e690f4e4621922256d2a5d48f997c1862
ebana <ebaerdenebat39@gmail.com>
2025-04-09 02:01:52 +0800

    pdf file view hiideg bolgosn

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ const zeelRouter = require('./router/Zeel')
 const cors = require('cors')
 var fs = require('fs')
 var path = require('path')
+const { fileURLToPath } = require('url');
 var accesLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),{flags:'a'})
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(errorHandler);
-
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(morgan('combined',{stream:accesLogStream}))
 app.use('/api/lib/author', authorRouter);
 app.use('/api/lib/durem', duremRouter);
@@ -33,10 +33,31 @@ app.use('/api/lib/category',categoryRouter);
 app.use('/api/lib/book',bookRouter);
 app.use('/api/lib/zahialga', zahialgaRouter);
 app.use('/api/lib/zeel', zeelRouter);
+
+app.get('/api/lib/pdf/download/:fileName', (req, res) => {
+  const { fileName } = req.params;
+  const filePath = path.join(__dirname, 'uploads', fileName);
+
+  // Файл байгаа эсэхийг шалгах
+  fs.existsSync(filePath) 
+    ? res.download(filePath) // Файлыг татах
+    : res.status(404).send('File not found');
+});
 const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
+app.get('/api/lib/pdf', (req, res) => {
+  const uploadsDir = path.join(__dirname, 'uploads');
+  fs.readdir(uploadsDir, (err, files) => {
+    if (err) {
+      return res.status(500).send('Error reading directory');
+    }
+    const pdfFiles = files.filter(file => path.extname(file).toLowerCase() === '.pdf');
+    res.json(pdfFiles);
+  });
+});
+
 const server = app.listen(
     process.env.PORT,
     console.log(`express server ${process.env.PORT}`)
